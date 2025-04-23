09edce070c5f034873f08f88831b3f853a3c504b
ADITYA PRATAP SINGH TOMAR <aditya.pratap.singh.tomar.1082006@gmail.com>
2025-04-24 00:58:46 +0530

    bot.py issue

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
-import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
-client = genai.GenerativeModel("gemini-1.5-pro")
-model = ChatGoogleGenerativeAI(client=client, temperature=0.5, max_output_tokens=1000)
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0.5,
+    max_output_tokens=1000,
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
 
 def predict_disease(symptoms, weight, height, gender, age):
     """Takes symptoms and health conditions, then predicts disease and suggests precautions."""
@@ -18,8 +21,7 @@ def predict_disease(symptoms, weight, height, gender, age):
     Age: {age} years
     Provide the two things:
     1. What is the probable disease that could be to this person and which type of disease is this.
-    2. What should he/she should do now ? Give First hand precations.
+    2. What should he/she should do now? Give first-hand precautions.
     """
     response = model.invoke(prompt)
     return response.content
-
